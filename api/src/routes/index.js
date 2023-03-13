ed88e992bb4756748ea9661e1eff2706e2e79579
mario19martinez <mariomartinez678abc@gmail.com>
2023-03-13 09:08:54 -0500

    Segundo commit

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,12 @@
 const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-
+const getDogs = require("./getDogs");
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
-
+router.use("/", getDogs);
 
 module.exports = router;
